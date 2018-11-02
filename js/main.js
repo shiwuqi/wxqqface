102aed610096cac437494cd01bfa8f68238dfebb
shiqi <liziw2012@163.com>
2018-11-02 14:07:07 +0800

    提交

---
+++ js/main.js
@@ -0,0 +1,282 @@
+var qqFaceArr2 = ["[微笑]", "[撇嘴]", "[色]", "[发呆]", "[大哭]", "[害羞]", "[闭嘴]", "[睡]", "[流泪]", "[尴尬]", "[发怒]", "[调皮]", "[呲牙]", "[惊讶]", "[难过]", "[冷汗]", "[抓狂]", "[吐]", "[偷笑]", "[可爱]", "[白眼]", "[傲慢]", "[饥饿]", "[困]", "[惊恐]", "[流汗]", "[憨笑]", "[大兵]", "[奋斗]", "[咒骂]", "[疑问]", "[嘘]", "[晕]", "[疯了]", "[衰]", "[敲打]", "[再见]", "[擦汗]", "[抠鼻]", "[糗大了]", "[坏笑]", "[左哼哼]", "[右哼哼]", "[哈欠]", "[鄙视]", "[委屈]", "[快哭了]", "[阴险]", "[亲亲]", "[吓]", "[可怜]", "[拥抱]", "[月亮]", "[太阳]", "[炸弹]", "[骷髅]", "[菜刀]", "[猪头]", "[西瓜]", "[咖啡]", "[饭]", "[爱心]", "[强]", "[弱]", "[握手]", "[胜利]", "[抱拳]", "[勾引]", "[OK]", "[NO]", "[玫瑰]", "[凋谢]", "[示爱]", "[爱情]", "[飞吻]"];
+var qqFaceArr = [{
+  "name": "[微笑]",
+  "value": "[em_1]"
+}, {
+  "name": "[撇嘴]",
+  "value": "[em_2]"
+}, {
+  "name": "[色]",
+  "value": "[em_3]"
+}, {
+  "name": "[发呆]",
+  "value": "[em_4]"
+}, {
+  "name": "[大哭]",
+  "value": "[em_5]"
+}, {
+  "name": "[害羞]",
+  "value": "[em_6]"
+}, {
+  "name": "[闭嘴]",
+  "value": "[em_7]"
+}, {
+  "name": "[睡]",
+  "value": "[em_8]"
+}, {
+  "name": "[流泪]",
+  "value": "[em_9]"
+}, {
+  "name": "[尴尬]",
+  "value": "[em_10]"
+}, {
+  "name": "[发怒]",
+  "value": "[em_11]"
+}, {
+  "name": "[调皮]",
+  "value": "[em_12]"
+}, {
+  "name": "[呲牙]",
+  "value": "[em_13]"
+}, {
+  "name": "[惊讶]",
+  "value": "[em_14]"
+}, {
+  "name": "[难过]",
+  "value": "[em_15]"
+}, {
+  "name": "[冷汗]",
+  "value": "[em_16]"
+}, {
+  "name": "[抓狂]",
+  "value": "[em_17]"
+}, {
+  "name": "[吐]",
+  "value": "[em_18]"
+}, {
+  "name": "[偷笑]",
+  "value": "[em_19]"
+}, {
+  "name": "[可爱]",
+  "value": "[em_20]"
+}, {
+  "name": "[白眼]",
+  "value": "[em_21]"
+}, {
+  "name": "[傲慢]",
+  "value": "[em_22]"
+}, {
+  "name": "[饥饿]",
+  "value": "[em_23]"
+}, {
+  "name": "[困]",
+  "value": "[em_24]"
+}, {
+  "name": "[惊恐]",
+  "value": "[em_25]"
+}, {
+  "name": "[流汗]",
+  "value": "[em_26]"
+}, {
+  "name": "[憨笑]",
+  "value": "[em_27]"
+}, {
+  "name": "[大兵]",
+  "value": "[em_28]"
+}, {
+  "name": "[奋斗]",
+  "value": "[em_29]"
+}, {
+  "name": "[咒骂]",
+  "value": "[em_30]"
+}, {
+  "name": "[疑问]",
+  "value": "[em_31]"
+}, {
+  "name": "[嘘]",
+  "value": "[em_32]"
+}, {
+  "name": "[晕]",
+  "value": "[em_33]"
+}, {
+  "name": "[疯了]",
+  "value": "[em_34]"
+}, {
+  "name": "[衰]",
+  "value": "[em_35]"
+}, {
+  "name": "[敲打]",
+  "value": "[em_36]"
+}, {
+  "name": "[再见]",
+  "value": "[em_37]"
+}, {
+  "name": "[擦汗]",
+  "value": "[em_38]"
+}, {
+  "name": "[抠鼻]",
+  "value": "[em_39]"
+}, {
+  "name": "[糗大了]",
+  "value": "[em_40]"
+}, {
+  "name": "[坏笑]",
+  "value": "[em_41]"
+}, {
+  "name": "[左哼哼]",
+  "value": "[em_42]"
+}, {
+  "name": "[右哼哼]",
+  "value": "[em_43]"
+}, {
+  "name": "[哈欠]",
+  "value": "[em_44]"
+}, {
+  "name": "[鄙视]",
+  "value": "[em_45]"
+}, {
+  "name": "[委屈]",
+  "value": "[em_46]"
+}, {
+  "name": "[快哭了]",
+  "value": "[em_47]"
+}, {
+  "name": "[阴险]",
+  "value": "[em_48]"
+}, {
+  "name": "[亲亲]",
+  "value": "[em_49]"
+}, {
+  "name": "[吓]",
+  "value": "[em_50]"
+}, {
+  "name": "[可怜]",
+  "value": "[em_51]"
+}, {
+  "name": "[拥抱]",
+  "value": "[em_52]"
+}, {
+  "name": "[月亮]",
+  "value": "[em_53]"
+}, {
+  "name": "[太阳]",
+  "value": "[em_54]"
+}, {
+  "name": "[炸弹]",
+  "value": "[em_55]"
+}, {
+  "name": "[骷髅]",
+  "value": "[em_56]"
+}, {
+  "name": "[菜刀]",
+  "value": "[em_57]"
+}, {
+  "name": "[猪头]",
+  "value": "[em_58]"
+}, {
+  "name": "[西瓜]",
+  "value": "[em_59]"
+}, {
+  "name": "[咖啡]",
+  "value": "[em_60]"
+}, {
+  "name": "[饭]",
+  "value": "[em_61]"
+}, {
+  "name": "[爱心]",
+  "value": "[em_62]"
+}, {
+  "name": "[强]",
+  "value": "[em_63]"
+}, {
+  "name": "[弱]",
+  "value": "[em_64]"
+}, {
+  "name": "[握手]",
+  "value": "[em_65]"
+}, {
+  "name": "[胜利]",
+  "value": "[em_66]"
+}, {
+  "name": "[抱拳]",
+  "value": "[em_67]"
+}, {
+  "name": "[勾引]",
+  "value": "[em_68]"
+}, {
+  "name": "[OK]",
+  "value": "[em_69]"
+}, {
+  "name": "[NO]",
+  "value": "[em_70]"
+}, {
+  "name": "[玫瑰]",
+  "value": "[em_71]"
+}, {
+  "name": "[凋谢]",
+  "value": "[em_72]"
+}, {
+  "name": "[示爱]",
+  "value": "[em_73]"
+}, {
+  "name": "[爱情]",
+  "value": "[em_74]"
+}, {
+  "name": "[飞吻]",
+  "value": "[em_75]"
+}]
+
+$(function () {
+  $(".wx_emotion_icon").qqFace({
+    id: 'facebox',
+    assign: 'emotion',
+    path: './images/icons/'
+  })
+})
+
+function submit() {// 提交
+  var content = $("#emotion").val();
+  $(".wx_content").html(replacePhoto(content));
+}
+
+function replaceEmotion(str) {// emotion转成表情文字(发送到后台)
+  str = str.replace(/\</g, '&lt;');
+  str = str.replace(/\>/g, '&gt;');
+  str = str.replace(/\n/g, '<br/>');
+  str = str.replace(/\[em_([0-9]*)\]/g, function (match, $1, $2, offset, source) {
+    console.log(match, $1, $2, offset, source);
+    var reStr = qqFaceArr2[Number($1) - 1];
+    return reStr;
+  });
+  return str;
+}
+
+function replacePhoto(str) {// [em_x]转成表情图片显示
+	str = str.replace(/\</g, '&lt;');
+	str = str.replace(/\>/g, '&gt;');
+	str = str.replace(/\n/g, '<br/>');
+	str = str.replace(/\[em_([0-9]*)\]/g, '<img src="./images/icons/$1.gif" border="0" style="width: 20px;height: 20px;margin: 0;vertical-align: middle;" />');
+	return str;
+}
+
+// 微信表情文字转化为表情图片
+function emotionChange(str) {
+	var pattern1 = /\[[\u4e00-\u9fa5]+\]/g;
+	var pattern2 = /\[[\u4e00-\u9fa5]+\]/;
+	var src = "";
+	var matchCont = str.match(pattern1);
+	if (!matchCont) {
+		return str;
+	} else {
+		for (var i = 0; i < matchCont.length; i++) {
+			for (var j = 0; j < qqFaceArr.length; j++) {
+				if (matchCont[i] == qqFaceArr[j].name) {
+					src = qqFaceArr[j].value;
+					break;
+				}
+			}
+			str = str.replace(pattern2, src);
+		}
+		return replacePhoto(str.replace(pattern2, src))
+	}
+}
